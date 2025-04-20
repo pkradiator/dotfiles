50663e9651d67f8faefbffbb526457bea322697c
pkradiator <kalalpriyanshu7@gmail.com>
2025-04-20 14:49:17 +0530

    Changed setup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,7 +22,7 @@
            gcs-done))
 
 (add-hook 'emacs-startup-hook #'pkd/display-startup-time)
-
+(load-theme 'modus-operandi-tinted)
 
 ;; if emacs version is less than 27, load the autoloads of files in load-path
 ;; is done automatically after version 27
@@ -40,15 +40,15 @@
 
 ;; Add melpa
 (add-to-list 'package-archives 
-    '("MELPA" .
-      "http://melpa.org/packages/"))
+	     '("MELPA" .
+	       "http://melpa.org/packages/"))
 
 ;; Full screen on startup
 (toggle-frame-fullscreen)
 
 ;; On Mac keyboard Command -> Meta
 (setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'control)
+;;(setq mac-option-modifier 'control)
 
 ;; I prefer spaces over tabs
 (setq indent-tabs-mode nil)
@@ -64,6 +64,14 @@
 
 ;; Show column number on the modeline.
 (column-number-mode)
+
+;;Use icomplete
+(use-package icomplete
+  :ensure nil
+  :demand t
+  :config
+  (fido-mode t))
+
 
 ;; Display line numbers in the left margin, as a general rule,
 (global-display-line-numbers-mode t)
@@ -117,8 +125,11 @@
   (modify-syntax-entry ?< ".")
   (modify-syntax-entry ?> "."))
 
+(add-hook 'org-mode-hook #'turn-on-org-cdlatex)
 (use-package org
-  :hook (org-mode . pkd/org-mode-setup)
+  :hook ((org-mode . pkd/org-mode-setup)
+	 (org-mode . turn-on-org-cdlatex))
+  
   :bind (("C-c l" . org-store-link)
 	 ("C-c a" . org-agenda)
 	 ("C-c c" . org-capture))
@@ -129,9 +140,9 @@
   ;; Org Capture templates
   (setq org-capture-templates
 	'(("t" "Todo" entry (file+headline "~/kp7/org/gtd.org" "Tasks")
-	   "* Todo %?\n  %i\n  %a")
-	  ("j" "Journal" entry (file+datetree "~/kp7/org/journal.org")
-	   "* %?\nEntered on %U\n  %i\n  %a")))
+	   "* TODO %?\n  %i\n")
+	  ("e" "Events" entry(file+headline "~/kp7/org/gtd.org" "Events")
+	   "* %?\n %T\n %i\n")))
   ;; Increase scale from 1.0 to 1.4
   (setq org-format-latex-options
 	'(:foreground default :background default :scale 1.4 :html-foreground "Black"
@@ -176,18 +187,29 @@
 (defun start-comp-prog-session ()
   "Starts a competitive programming session"
   (interactive)
-  (delete-other-windows)
-  (find-file "~/kp7/Competitive_Programming/cpp_env/sol.cpp")
-  (split-window-right)
-  (other-window 1)
-  (find-file "~/kp7/Competitive_Programming/cpp_env/input.txt")
-  (split-window-below)
-  (enlarge-window -9)
-  (other-window 1)
-  (split-window-below)
-  (find-file "~/kp7/Competitive_Programming/cpp_env/output.txt")
-  (other-window 1)
-  (eat))
+  (if (> (window-width) 130)
+      (progn
+	(delete-other-windows)
+	(find-file "~/kp7/Competitive_Programming/cpp_env/sol.cpp")
+	(split-window-right)
+	(other-window 1)
+	(find-file "~/kp7/Competitive_Programming/cpp_env/input.txt")
+	(split-window-below)
+	(enlarge-window -9)
+	(other-window 1)
+	(split-window-below)
+	(find-file "~/kp7/Competitive_Programming/cpp_env/output.txt")
+	(other-window 1)
+	(eat))
+    (progn
+      (delete-other-windows)
+      (find-file "~/kp7/Competitive_Programming/cpp_env/sol.cpp")
+      (split-window-below)
+      (enlarge-window 20)
+      (other-window 1)
+      (find-file "~/kp7/Competitive_Programming/cpp_env/input.txt")
+      (eat)
+      (other-window 1))))
 (keymap-global-set "C-x c p" 'start-comp-prog-session)
 
 (use-package smartparens
@@ -203,12 +225,12 @@
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
 (use-package pdf-tools
-      :ensure t
-      :commands pdf-view-mode
-      :mode ("\\.pdf\\'" . pdf-view-mode)
-      :hook ((pdf-view-mode . (lambda () (display-line-numbers-mode -1) (auto-revert-mode))))
-      :config
-      (pdf-loader-install))
+  :ensure t
+  :commands pdf-view-mode
+  :mode ("\\.pdf\\'" . pdf-view-mode)
+  :hook ((pdf-view-mode . (lambda () (display-line-numbers-mode -1) (auto-revert-mode))))
+  :config
+  (pdf-loader-install))
 
 (use-package yaml-mode
   :ensure t)
@@ -224,11 +246,12 @@
    ("\\.erb\\'" . web-mode)
    ("\\.mustache\\'" . web-mode)
    ("\\.djhtml\\'" . web-mode)
-   ("\\.jsx\\'" . web-mode)
-   ("\\.html\\'" . web-mode)
-   ("\\.js\\'" . web-mode)
-   ("\\.css\\'" . web-mode)
-   ("\\.tsx\\'" . web-mode))
+   ;; ("\\.jsx\\'" . web-mode)
+   ;; ("\\.html\\'" . web-mode)
+   ;; ("\\.js\\'" . web-mode)
+   ;; ("\\.css\\'" . web-mode)
+   ;; ("\\.tsx\\'" . web-mode)
+   )
   :config
   (setq web-mode-markup-indent-offset 2
 	web-mode-code-indent-offset 2
@@ -236,4 +259,105 @@
 
 (use-package magit
   :ensure t
-  :bind ("C-x g" . magit-status))
+  :bind ("C-x g g" . magit-status))
+
+(use-package dired
+  :ensure nil
+  :config
+  (setq dired-kill-when-opening-new-dired-buffer 1))
+
+(use-package treesit
+  :mode (("\\.tsx\\'" . tsx-ts-mode)
+         ("\\.js\\'"  . typescript-ts-mode)
+         ("\\.mjs\\'" . typescript-ts-mode)
+         ("\\.mts\\'" . typescript-ts-mode)
+         ("\\.cjs\\'" . typescript-ts-mode)
+         ("\\.ts\\'"  . typescript-ts-mode)
+         ("\\.jsx\\'" . tsx-ts-mode)
+         ("\\.json\\'" .  json-ts-mode)
+         ("\\.Dockerfile\\'" . dockerfile-ts-mode)
+         ("\\.prisma\\'" . prisma-ts-mode)
+         ;; More modes defined here...
+         )
+  :preface
+  (defun os/setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+               (bash "https://github.com/tree-sitter/tree-sitter-bash")
+               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+               (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
+               (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+               (make "https://github.com/alemuller/tree-sitter-make")
+               (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+               (cmake "https://github.com/uyha/tree-sitter-cmake")
+               (c "https://github.com/tree-sitter/tree-sitter-c")
+               (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+               (toml "https://github.com/tree-sitter/tree-sitter-toml")
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+               (prisma "https://github.com/victorhqc/tree-sitter-prisma")))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it
+      ;; installed. However, if you want to *update* a grammar then
+      ;; this obviously prevents that from happening.
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
+
+  ;; Optional, but recommended. Tree-sitter enabled major modes are
+  ;; distinct from their ordinary counterparts.
+  ;;
+  ;; You can remap major modes with `major-mode-remap-alist'. Note
+  ;; that this does *not* extend to hooks! Make sure you migrate them
+  ;; also
+  (dolist (mapping
+           '((python-mode . python-ts-mode)
+             (css-mode . css-ts-mode)
+             (typescript-mode . typescript-ts-mode)
+             (js-mode . typescript-ts-mode)
+             (js2-mode . typescript-ts-mode)
+             (c-mode . c-ts-mode)
+             (c++-mode . c++-ts-mode)
+             (c-or-c++-mode . c-or-c++-ts-mode)
+             (bash-mode . bash-ts-mode)
+             (css-mode . css-ts-mode)
+             (json-mode . json-ts-mode)
+             (js-json-mode . json-ts-mode)
+             (sh-mode . bash-ts-mode)
+             (sh-base-mode . bash-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping))
+  :config
+  (os/setup-install-grammars))
+
+
+(use-package elfeed
+  :ensure t
+  :hook
+  (elfeed-show-mode . visual-line-mode)
+  :bind (("C-x w w" . elfeed))
+  :config
+  (setq elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory))
+  (setq elfeed-feeds
+	'(;; Blogs
+	  "http://nullprogram.com/feed/"
+	  "https://lexi-lambda.github.io/feeds/all.rss.xml"
+	  "https://justanotherelectronicsblog.com/?feed=rss2"
+	  "https://scripter.co/atom.xml"
+	  "https://eugene-andrienko.com/en/feed.xml"
+	  "https://karthinks.com/index.xml"
+	  "https://pointersgonewild.com/feed.xml"
+	  )))
+(use-package ibuffer
+  :ensure nil
+  :bind (("C-x C-b" . ibuffer-other-window)))
+
+(use-package latex-preview-pane
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t)
